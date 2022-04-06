cdd7c5cc26eec20d447fc74c33f94de533e3d33c
Tuan Dang <66971925+tmdang1101@users.noreply.github.com>
2022-04-06 12:36:00 -0500

    added category display

---
+++ knox-marketplace/src/data.js
@@ -0,0 +1,17 @@
+export const categories = [
+    {
+      id: 1,
+      img: "https://www.nvi.com.au/images/transform/v1/crop/frm/y8YKNWH3Pdv2guZsVFQpjk/e77733fe-b6ca-42f3-aedc-59f5c72354fe.jpg/r811_0_1920_1280_w1200_h678_fmax.jpg",
+      title: "BOOKS",
+    },
+    {
+      id: 2,
+      img: "https://i.ebayimg.com/00/s/MTAxM1gxMDEz/z/w5EAAOSwYy1iRJdr/$_10.JPG?set_id=880000500F",
+      title: "CLOTHING",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/originals/df/ac/3a/dfac3a926b9c7b878620f37d0d4d117f.jpg",
+      title: "ARTWORK",
+    },
+  ];
