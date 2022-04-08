f872f37a5baa94f7526a02ebcb450f2e4b7c699f
Hector S <75858294+hectorsally29@users.noreply.github.com>
2022-04-08 13:18:46 -0500

    fixed merge conflict

--- knox-marketplace/src/pages/Home.jsx
+++ knox-marketplace/src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import Categories from "../components/Categories";
+import '../App.css';
+import LoginButton from "../components/login";
+import LogoutButton from "../components/logout";
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const clientId = "961228782855-iluvtmr6tvcpkrg5a6idrutbj6gt5vdl.apps.googleusercontent.com";
 
 const Home = () => {
+    useEffect(() => {
+        function start(){
+            gapi.client.init({
+              clientId: clientId,
+              scope: ""
+            })
+        };
+    
+        gapi.load('client:auth2', start);
+      });
+
     return (
-        <div>
+        <div className="App">
+            <LogoutButton />
+            <LoginButton />
             <Categories/>
         </div>
     );
