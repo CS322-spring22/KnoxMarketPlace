3a7a1a8835ec5f89404b2c3dd4dd3a483773ed9e
Hector S <75858294+hectorsally29@users.noreply.github.com>
2022-04-08 12:29:58 -0500

    Google Login and Logout
    added google login and logout, also edited the App.js

--- knox-marketplace/src/App.js
+++ knox-marketplace/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import LoginButton from "./HectorComponents/login";
+import LogoutButton from "./HectorComponents/logout";
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const clientId = "961228782855-iluvtmr6tvcpkrg5a6idrutbj6gt5vdl.apps.googleusercontent.com";
+
 
 function App() {
+
+  useEffect(() => {
+    function start(){
+        gapi.client.init({
+          clientId: clientId,
+          scope: ""
+        })
+    };
+
+    gapi.load('client:auth2', start);
+  });
+
   return (
-    // BEM
-    <div className="app">
-      <h1>Knox Marketplace</h1> 
+    <div className="App">
+      <LogoutButton />
+      <LoginButton />
     </div>
   );
 }
