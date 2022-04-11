5221fd954f173b5bd87a3abbcaa5aa318aeeb8f2
Tuan Dang <66971925+tmdang1101@users.noreply.github.com>
2022-04-11 12:52:03 -0500

    put Sign In button inside Navigation Bar

--- knox-marketplace/src/components/Navbar.jsx
+++ knox-marketplace/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
     height: 60px;
@@ -31,17 +32,38 @@ const Right = styled.div`
 `
 
 export const Navbar = () => {
-  return (
-    <Container>
-        <Wrapper>
-            <Left>Post Item</Left>
-            <Center>
-                <OurName>Knox Marketplace</OurName>
-            </Center>
-            <Right>Cart Login</Right>
-        </Wrapper>
-    </Container>
-  )
+    let navigate = useNavigate();
+
+    return (
+        <Container>
+            <Wrapper>
+                <Left>Post Item</Left>
+                <Center>
+                    <OurName>Knox Marketplace</OurName>
+                </Center>
+                <Right><HomeSignUp onClick={() => {navigate("/loginPage")}}>Sign in</HomeSignUp></Right>
+            </Wrapper>
+        </Container>
+    )
 }
+
+const HomeSignUp = styled.button`
+    right: 0px;
+    appearance: none;
+    background-color: #000000;
+    border: 2px solid #1A1A1A;
+    border-radius: 5px;
+    box-sizing: border-box;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: normal;
+    min-height: 40px;
+    outline: none;
+    padding: 6px 8px;
+    text-align: center;
+ `;
 
 export default Navbar
--- knox-marketplace/src/pages/Home.jsx
+++ knox-marketplace/src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import '../App.css';
 import styled from 'styled-components'
-import { useNavigate } from "react-router-dom";
 
 const Page = styled.div`
     margin: 0;
@@ -27,8 +26,6 @@ const Right = styled.div`
 
 const Home = () => {
 
-    let navigate = useNavigate();
-
     return (
         <div className="App">
             <Page>
@@ -37,33 +34,11 @@ const Home = () => {
                 </Left>
                 <Right>
                     <Navbar/>
-                    <HomeSignUp onClick={() => {navigate("/loginPage")}}>Hello, Sign in or Register</HomeSignUp>
                     <Categories/>
                 </Right>
             </Page>
         </div>
     )
 };
-
-const HomeSignUp = styled.button`
-    position: absolute;
-    top: 0px;
-    right: 0px;
-    appearance: none;
-    background-color: #000000;
-    border: 2px solid #1A1A1A;
-    border-radius: 15px;
-    box-sizing: border-box;
-    color: #FFFFFF;
-    cursor: pointer;
-    display: inline-block;
-    font-size: 16px;
-    font-weight: 600;
-    line-height: normal;
-    min-height: 60px;
-    outline: none;
-    padding: 16px 24px;
-    text-align: center;
- `;
 
 export default Home;
