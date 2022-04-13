67902fd52f668ea459aa5cb33a709b35121f4a7a
Hector S <75858294+hectorsally29@users.noreply.github.com>
2022-04-13 14:04:45 -0500

    Back end for sign up
    You are now able to register an account and it will go to the database of firebase. If the credentials are correct it will log you in.

--- knox-marketplace/src/App.jsx
+++ knox-marketplace/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from 'react';
 import LoginPage from "./pages/loginPage"; 
 import Home from "./pages/Home";
 import styled from "styled-components";
 import Register from "./pages/register";
+
+
+
 
 
 const App = () => {
---
+++ knox-marketplace/src/firebase.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKigeU6nrudM6-Tc4QQjbdsY_IxBcDHlM",
+    authDomain: "knox-marketplace.firebaseapp.com",
+    projectId: "knox-marketplace",
+    storageBucket: "knox-marketplace.appspot.com",
+    messagingSenderId: "786295959114",
+    appId: "1:786295959114:web:a8cc959134e2f723fe80b3",
+    measurementId: "G-ZY4L2PLT7J"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  
+  export { db, auth };
